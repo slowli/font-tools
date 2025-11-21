f342c0c367812ed08d8d60c988233b3521aecd5d
Alex Ostrovski <ostrovski.alex@gmail.com>
2025-11-21 20:03:59 +0200

    Sketch CLI subset command

--- crates/font-subset-cli/src/main.rs
+++ crates/font-subset-cli/src/main.rs
@@ -1,6 +1,13 @@
 //! `font-subset` command-line application.
 
-use std::{fmt, fs, io, io::Read, path::PathBuf};
+use std::{
+    collections::BTreeSet,
+    fmt, fs, io,
+    io::{Read, Write},
+    ops,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 use anstream::{print, println};
 use anstyle::{AnsiColor, Color, Style};
@@ -67,6 +74,45 @@ impl fmt::Display for Checkbox {
     }
 }
 
+#[derive(Debug, Clone)]
+struct CharRange(ops::RangeInclusive<char>);
+
+impl FromStr for CharRange {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // FIXME: parse regex-like syntax: a-zA-Z!,0-9\u{77a}
+
+        let (start, end) = s
+            .split_once('-')
+            .context("range does not contain `-` delimiter")?;
+        let start = parse_char(start)
+            .with_context(|| format!("invalid start char of the range {start:?}"))?;
+        let end =
+            parse_char(end).with_context(|| format!("invalid end char of the range {end:?}"))?;
+
+        Ok(Self(start..=end))
+    }
+}
+
+fn parse_char(s: &str) -> anyhow::Result<char> {
+    const INVALID_FORMAT: &str = "invalid code point format; should be \\u{<hex>}";
+
+    // Check for a single-char string.
+    let mut s_chars = s.chars();
+    let first_char = s_chars.next().context("empty")?;
+    if s_chars.next().is_none() {
+        return Ok(first_char);
+    }
+
+    // Check for \u|U{..}
+    let code = s.strip_prefix("\\u{").or_else(|| s.strip_prefix("\\U{"));
+    let code = code.context(INVALID_FORMAT)?;
+    let code = code.strip_suffix('}').context(INVALID_FORMAT)?;
+    let code = u32::from_str_radix(code, 16).context(INVALID_FORMAT)?;
+    char::from_u32(code).with_context(|| format!("{code} is not a valid Unicode char code"))
+}
+
 #[derive(Debug, Parser)]
 struct Cli {
     #[command(subcommand)]
@@ -77,30 +123,112 @@ struct Cli {
 enum CliCommand {
     /// Prints information about a font.
     Info {
-        #[command(flatten)]
-        shared: SharedArgs,
+        /// Path to the font file, or `-` to read from stdin.
+        path: PathBuf,
     },
+    /// Subsets a font.
+    Subset(SubsetCommand),
 }
 
 #[derive(Debug, Clone, Args)]
-struct SharedArgs {
+struct SubsetCommand {
+    /// Subset the ASCII chars (' ' / 0x20 to '~' / 0x7e).
+    #[arg(long)]
+    ascii: bool,
+    /// Chars to include in the subset.
+    #[arg(long, short = 'C')]
+    chars: Vec<String>,
+    /// Char ranges to include in the subset.
+    #[arg(long, short = 'R')]
+    ranges: Vec<CharRange>,
+
+    /// Output format. If not specified, will be determined by the output path extension.
+    #[arg(long)]
+    format: Option<FontFormat>,
+    /// Forces subsetting even if the font permissions don't support it.
+    #[arg(long)]
+    force: bool,
+    /// Allows chars mapped to the undefined glyph.
+    #[arg(long)]
+    allow_missing: bool,
+    /// Path to the output font, or `-` to print to stdout.
+    #[arg(long = "out", short = 'o', value_name = "PATH")]
+    output: PathBuf,
     /// Path to the font file, or `-` to read from stdin.
     path: PathBuf,
 }
 
-impl SharedArgs {
-    fn read_font<'font>(&self, buffer: &'font mut Vec<u8>) -> anyhow::Result<FontReader<'font>> {
-        if self.path.as_os_str() == "-" {
-            io::stdin()
-                .read_to_end(buffer)
-                .context("cannot read font bytes from stdin")?;
+impl SubsetCommand {
+    fn run(self) -> anyhow::Result<()> {
+        let format = if let Some(format) = self.format {
+            format
         } else {
-            *buffer = fs::read(&self.path).with_context(|| {
-                format!("cannot read font bytes from `{}`", self.path.display())
+            FontFormat::detect(&self.output).with_context(|| {
+                format!(
+                    "cannot detect font format for output path `{}`",
+                    self.output.display()
+                )
+            })?
+        };
+
+        let mut buffer = vec![];
+        let font_reader = read_font(&self.path, &mut buffer)?;
+        let font = font_reader.read().context("failed parsing font")?;
+
+        anyhow::ensure!(
+            self.force || font.permissions().allow_subsetting,
+            "font permissions do not allow subsetting"
+        );
+
+        let mut chars = BTreeSet::new();
+        if self.ascii {
+            chars.extend(' '..='~');
+        }
+        chars.extend(self.chars.iter().flat_map(|s| s.chars()));
+        chars.extend(self.ranges.iter().flat_map(|range| range.0.clone()));
+
+        anyhow::ensure!(!chars.is_empty(), "The char subset is empty");
+
+        if !self.allow_missing {
+            let missing_chars: Vec<_> = chars
+                .iter()
+                .copied()
+                .filter(|&ch| !font.contains_char(ch))
+                .collect();
+            anyhow::ensure!(
+                missing_chars.is_empty(),
+                "Source font does not contain some of subset chars: {missing_chars:?}"
+            );
+        }
+
+        let subset = font.subset(&chars).context("failed subsetting")?;
+        let output_bytes = match format {
+            FontFormat::OpenType => subset.to_opentype(),
+            FontFormat::Woff2 => subset.to_woff2(),
+        };
+        if self.output.as_os_str() == "-" {
+            io::stdout()
+                .write_all(&output_bytes)
+                .context("failed writing subset font to stdout")?;
+        } else {
+            fs::write(&self.output, &output_bytes).with_context(|| {
+                format!("failed writing subset font to `{}`", self.output.display())
             })?;
         }
-        FontReader::new(&*buffer).context("failed parsing font header")
+        Ok(())
     }
+}
+
+fn read_font<'font>(path: &Path, buffer: &'font mut Vec<u8>) -> anyhow::Result<FontReader<'font>> {
+    if path.as_os_str() == "-" {
+        io::stdin()
+            .read_to_end(buffer)
+            .context("cannot read font bytes from stdin")?;
+    } else {
+        *buffer = fs::read(path)
+            .with_context(|| format!("cannot read font bytes from `{}`", path.display()))?;
+    }
+    FontReader::new(&*buffer).context("failed parsing font header")
 }
 
 #[derive(Debug, Clone, Copy, ValueEnum)]
@@ -109,12 +237,22 @@ enum FontFormat {
     Woff2,
 }
 
+impl FontFormat {
+    fn detect(path: &Path) -> Option<Self> {
+        Some(match path.extension()?.to_str()? {
+            "otf" | "ttf" => Self::OpenType,
+            "woff" | "woff2" => Self::Woff2,
+            _ => return None,
+        })
+    }
+}
+
 impl Cli {
     fn run(self) -> anyhow::Result<()> {
         match self.command {
-            CliCommand::Info { shared } => {
+            CliCommand::Info { path } => {
                 let mut buffer = vec![];
-                let font_reader = shared.read_font(&mut buffer)?;
+                let font_reader = read_font(&path, &mut buffer)?;
                 let font = font_reader.read().context("failed parsing font")?;
                 Self::print_font_naming(&font);
                 println!();
@@ -126,6 +264,7 @@ impl Cli {
                 println!();
                 Self::print_table_stats(&font_reader);
             }
+            CliCommand::Subset(cmd) => cmd.run()?,
         }
         Ok(())
     }
